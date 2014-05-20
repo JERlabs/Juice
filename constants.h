fb707abff4f4021b39e99505b315a53057f8738a
Raj S. <rajshrimali@gmail.com>
2014-05-20 19:37:39 -0400

    Documentation for Constants.h

--- constants.h
+++ constants.h
@@ -1,3 +1,8 @@
+/** This file provides some definitions which are used in a lot of programs. 
+ * It defines the elementary charge, K in Coulumbs law, and a conversion factor for coulumbs. 
+ * This should be included by every other class, so there is no reason to include this direcly. 
+ */
+
 #ifndef CONST_H
 #define CONST_H 
 
@@ -5,5 +10,6 @@
 #define ELECTC 0.0000000000000000001602
 #define K 8987551787.3681764
 #define UC_IN_C 1000000
+#define C_IN_UC (1/1000000)
 
 #endif 
