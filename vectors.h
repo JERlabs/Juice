1766fe800ababdbcf92fe24a0f77bc29e6b396e2
Raj S. <rajshrimali@gmail.com>
2014-05-20 10:51:15 -0400

    Documentation for Vector Member Values
    Docs, because school is boring.

--- vectors.h
+++ vectors.h
@@ -3,10 +3,20 @@
 
 
 #include "constants.h"
+/** Class for Vecotor Values
+ * This is used for a variety of purposes. 
+ * It is used for Points, Forces, magntidudes, etc. 
+ * Relatively simple class, 
+ * In some ways more like a point, 
+ * but implementation should be limited to velocities, forces, and real vectgr values
+ * Although this CAN be used as a point, you should use a special point class for point values.
+ * Internally, it holds all Vectors as an X and Y magntiude. Also holds an angle, which is just used for calculating radian vectors. 
+ * The magnitude is reccalculated whenever called. 
+ */ 
 class Vector{
-    double mag_x; 
-    double mag_y; 
-    double angle; 
+    double mag_x; /**< hodls the x magnitude of all vectors */  
+    double mag_y; /**< holds the x-magnitude of all vectors.*/ 
+    double angle; /**< holds the angle for all vectors. This is semi-redundant. */ 
     void calcNewAngle(); 
     public:  
         Vector(); 
